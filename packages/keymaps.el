0662d2ec916fdf739b60c8cc6b189d03921743aa
Samuel Jackson <dsiq3g@gmail.com>
2025-03-02 23:48:03 -0800

    Add which-key functions to help map

--- packages/keymaps.el
+++ packages/keymaps.el
@@ -1,10 +1,25 @@
 (use-package which-key
 	:ensure t
 	:hook (after-init . which-key-mode)
+	:custom
+		(which-key-allow-evil-operators t)
+		(which-key-show-operator-state-maps t)
 	:config
-		(setq which-key-allow-evil-operators t
-			  which-key-show-operator-state-maps t)
-		(which-key-mode))
+		(setq which-key-map (make-sparse-keymap))
+		(define-key help-map (kbd "W") (cons "which-key"
+			(let ((which-key-map (make-sparse-keymap)))
+				(define-key which-key-map (kbd "k") (cons "select keymap" #'which-key-show-keymap))
+				(define-key which-key-map (kbd "t") (cons "top level" #'which-key-show-top-level))
+				(define-key which-key-map (kbd "m") (cons "major mode" #'which-key-show-major-mode))
+				(define-key which-key-map (kbd "M") (cons "minor mode" #'which-key-show-minor-mode-keymap))
+
+				(define-key which-key-map (kbd "f") (cons "full maps"
+					(let ((full-maps (make-sparse-keymap)))
+						(define-key which-key-map (kbd "") (cons "" #'which-key-show-full-keymap))
+						(define-key which-key-map (kbd "") (cons "" #'which-key-show-full-major-mode))
+						(define-key which-key-map (kbd "") (cons "" #'which-key-show-full-minor-mode-keymap))
+						full-maps)))
+				which-key-map))))
 
 (use-package which-key
 	:ensure nil
@@ -33,7 +48,8 @@
 
 		; Make new mappings
 		(setq toggle-leader-keymap (make-sparse-keymap))
-		(define-key leader-keymap (kbd "t") (cons "toggle" toggle-leader-keymap))
+		(setq toggle-leader-key (kbd "t"))
+		(define-key leader-keymap toggle-leader-key (cons "toggle" toggle-leader-keymap))
 		(provide 'toggle-leader-keymapping)
 
 		(setq find-leader-keymap (make-sparse-keymap))
