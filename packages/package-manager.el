fba604c8b62fe2a9d2f35e30efbbd6eeddb55052
Samuel Jackson <dsiq3g@gmail.com>
2025-03-02 23:47:26 -0800

    Add todo items

---
+++ packages/package-manager.el
@@ -0,0 +1,6 @@
+;; Although elpaca should already be installed through bootstrapping, this section
+;; is for adding packages or settings ment to make working with elpaca Easier.
+
+;; (use-package melpulls
+;; 	:ensure t
+;; 	:after elpaca)
