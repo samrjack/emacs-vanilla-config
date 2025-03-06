50e575d420a4e919d13afb187727701fba034a97
Samuel Jackson <dsiq3g@gmail.com>
2025-03-05 23:25:43 -0800

    Add line numbers

---
+++ packages/programming/general.el
@@ -0,0 +1,8 @@
+(use-package emacs
+	:ensure nil
+	:hook (prog-mode . display-line-numbers-mode))
+(use-package emacs
+	:ensure nil
+	:after toggle-leader-keymapping
+	:config
+		(define-key toggle-leader-keymap (kbd "n") (cons "line numbers" #'display-line-numbers-mode)))
