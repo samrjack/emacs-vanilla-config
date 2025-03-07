13863253e6eaaa0e3dde8b6fd88b1ca745942ccf
Samuel Jackson <dsiq3g@gmail.com>
2025-03-06 21:35:31 -0800

    Add elpaca ui

--- packages/package-manager.el
+++ packages/package-manager.el
@@ -1,6 +1,7 @@
 ;; Although elpaca should already be installed through bootstrapping, this section
 ;; is for adding packages or settings ment to make working with elpaca Easier.
 
-;; (use-package melpulls
-;; 	:ensure t
-;; 	:after elpaca)
+(use-package melpulls
+	:ensure (:host github :repo "progfolio/melpulls")
+	:after elpaca
+	:config (add-to-list 'elpaca-menu-functions #'melpulls))
